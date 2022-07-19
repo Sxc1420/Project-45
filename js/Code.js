450f520ae9f21f1747599a998140aacbf09e5d5d
Sxc1420 <107283128+Sxc1420@users.noreply.github.com>
2022-07-19 17:09:55 -0500

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,23 @@
+const accessCode1 = "SPRITE";
+const accessCode2 = "ARRAY";
+const accessCode3 = "OPERATORS";
+
+function clues() {
+    fill("white");
+    textSize(15);
+    text("R S E T I P", 100,50);
+    fill("#404040");
+    text("Hint: Possesses the ability to move !!", 100,70);
+
+    fill("white");
+    textSize(15);
+    text("R Y A R A", 700,150);
+    fill("#404040");
+    text("Hint: Consists of a collection of elements !!", 700,170);
+
+    fill("white");
+    textSize(15);
+    text("R T O S O R P A E", 100,250);;
+    fill("#404040");
+    text("Hint: Symbols that represent an action !!", 100,270);
+}
